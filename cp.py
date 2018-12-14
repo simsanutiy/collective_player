48e61c8e85cb4156ab79c343464f1734919e1f83
Михалыч <simsanutiy@gmail.com>
2018-12-15 00:18:46 +0400

    Funny comments added

--- cp.py
+++ cp.py
@@ -1,16 +1,17 @@
+"""Грузим туеву хучу рандомных модулей"""
 import msvcrt
 import youtube_dl
 import subprocess
 import os
 
-order = 0
-play_order = 1
-done = None
-queue = {}
-query = ''
-playing = False
-f = open('text.txt', 'w')
+"""Некоторые переменный надо объявить заранее, до взода в цикл"""
+order = 0       #Номер последнего скачанного трека
+play_order = 1  #Номер текущего трека
+done = None     #Условие выхода из цикла
+query = ''      #Текст запроса
+playing = False #Играет ли музыка? А? А? Хуй на!
 
+"""Загрузка видео"""
 def download_video(order, query):
     ydl_opts = {
         'default_search': 'ytsearch1',
@@ -19,43 +20,49 @@ def download_video(order, query):
         'quiet': True,
         'get_title': True,
         'outtmpl': '{0}.tmp'.format(order),
-        }
+        }       #Опции загрузчика YoutubeDL, очевидно. А вот и сам загрузчик:
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([query])
 
+"""Главный цикл"""
 while not done:
-
+    
     """Неблокирующий ввод с клавиатуры"""
-    if msvcrt.kbhit():
-        button = msvcrt.getch()
-        letter = button.decode('cp866')
-        if letter == '\r':
-            order += 1
-            queue[order] = query
-            print('\r')
-            download_video(order, query)
-            query = ''
-            continue
-        if button == b'\x08':
-            query = query[:-1]
-            print(letter, sep=' ', end='', flush=True)
-            print(' ', sep=' ', end='', flush=True)
-            print(letter, sep=' ', end='', flush=True)
-            continue
-        query += letter
-        print(letter, sep=' ', end='', flush=True)
+    
+    if msvcrt.kbhit():#Если кнопка нажата,
+        button = msvcrt.getch()#то какая это кнопка
+        letter = button.decode('cp866')#и что она вообще значит?
+        if letter == '\r':#Если это Enter,
+            order += 1#то у нас на одну песню больше в плейлисте,
+            print('\r')#у нас новая строка,
+            download_video(order, query)#и пора бы уже скачать саму песню,
+            query = ''#забыть всё, что было,
+            continue#и идти дальше, пропустив всё что там идёт потом в цикле.
+        
+        if button == b'\x08':#А если это Backspace,
+            query = query[:-1]#то последняя буква нам уже не нужна.
+            print(letter, sep=' ', end='', flush=True)#В строке тоже. Шаг назад.
+            print(' ', sep=' ', end='', flush=True)#Закрыть пробелом.
+            print(letter, sep=' ', end='', flush=True)#Ещё шаг назад.
+            continue#Отпустить и забыть.
+        
+        #А если всё, что было выше, и не происходило вовсе?
+        query += letter#Тогда мы просто добавляем букву к запросу
+        print(letter, sep=' ', end='', flush=True)#и печатаем её.
 
     """Переключение на следующий трек и остановка воспроизведения"""
-    if playing:
-        poll = music.poll()
-        if poll != None:
-            os.remove('{0}.tmp'.format(play_order))
-            play_order += 1
-            if play_order > order:
-                print('Конец плейлиста')
-                break
-            playing = False
+    if playing:#Если говорят, что музыка играет,
+        poll = music.poll()#то стоит в этом убедиться,
+        if poll != None:#ведь если уже не играет,
+            os.remove('{0}.tmp'.format(play_order))#то и песня нам уже не нужна,
+            play_order += 1#мы просто включим следующую.
+            if play_order > order:#А если следующей нет,
+                print('Конец плейлиста')#то мы прямо об этом заявим
+                break#и вырвемся из этого долгого цикла. I want to break free!
+            playing = False#Ну и надо предупредить, что песня-то кончилась!
             
-    if not playing and not order == 0:
-        music = subprocess.Popen('ffplay {0}.tmp -autoexit -loglevel warning'.format(play_order))
-        playing = True
+    if not playing and not order == 0:#А если говорят, что музыка не играет,
+        music = subprocess.Popen('ffplay {0}.tmp -autoexit -loglevel warning'\
+                                 .format(play_order))#то включаем
+        #какая там сейчас должна быть песня.
+        playing = True#И громко всем об этом заявим!
