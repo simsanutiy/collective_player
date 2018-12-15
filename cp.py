c46070c24e89eec8c134939a672414a6dc540238
Михалыч <simsanutiy@gmail.com>
2018-12-15 20:15:58 +0400

    Harder, better, faster, stronger

--- cp.py
+++ cp.py
@@ -1,10 +1,11 @@
 """Грузим туеву хучу рандомных модулей"""
-import msvcrt
-import youtube_dl
-import subprocess
-import os
+import msvcrt               #Модуль для кнопок в командной строке
+import youtube_dl           #Модуль для качания видосиков с ютьюба
+from subprocess import Popen#Модуль для запуска плеера
+from os import remove       #Модуль для удаления отыгранных песен
+from time import sleep      #Модуль баиньки
 
-"""Некоторые переменный надо объявить заранее, до взода в цикл"""
+"""Некоторые переменные надо объявить заранее, до входа в цикл"""
 order = 0       #Номер последнего скачанного трека
 play_order = 1  #Номер текущего трека
 done = None     #Условие выхода из цикла
@@ -13,16 +14,15 @@ playing = False #Играет ли музыка? А? А? Хуй на!
 
 """Загрузка видео"""
 def download_video(order, query):
-    ydl_opts = {
-        'default_search': 'ytsearch1',
-        'format': 'bestaudio/best',
-        'audio_format': 'mp3',
-        'quiet': True,
-        'get_title': True,
-        'outtmpl': '{0}.tmp'.format(order),
-        }       #Опции загрузчика YoutubeDL, очевидно. А вот и сам загрузчик:
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([query])
+    ydl_opts = {#Опции загрузчика таковы:
+        'default_search': 'ytsearch1',#Первое видео из результатов поиска.
+        'format': 'bestaudio/best',#Скачивает лучшую звуковую дорожку или видео.
+        'audio_format': 'mp3',#В формате mp3.
+        'quiet': True,#Не пишет в консоль ничего.
+        'outtmpl': '{0}) {1}.tmp'.format(order, query),#Имя файла.
+        }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:#И с этими опциями наперевес
+        ydl.download([query])#скачать видео.
 
 """Главный цикл"""
 while not done:
@@ -37,7 +37,7 @@ while not done:
             print('\r')#у нас новая строка,
             download_video(order, query)#и пора бы уже скачать саму песню,
             query = ''#забыть всё, что было,
-            continue#и идти дальше, пропустив всё что там идёт потом в цикле.
+            continue#и идти дальше, пропустив всё, что там идёт потом в цикле.
         
         if button == b'\x08':#А если это Backspace,
             query = query[:-1]#то последняя буква нам уже не нужна.
@@ -54,7 +54,7 @@ while not done:
     if playing:#Если говорят, что музыка играет,
         poll = music.poll()#то стоит в этом убедиться,
         if poll != None:#ведь если уже не играет,
-            os.remove('{0}.tmp'.format(play_order))#то и песня нам уже не нужна,
+            remove('{0}.tmp'.format(play_order))#то и песня нам уже не нужна,
             play_order += 1#мы просто включим следующую.
             if play_order > order:#А если следующей нет,
                 print('Конец плейлиста')#то мы прямо об этом заявим
@@ -62,7 +62,9 @@ while not done:
             playing = False#Ну и надо предупредить, что песня-то кончилась!
             
     if not playing and not order == 0:#А если говорят, что музыка не играет,
-        music = subprocess.Popen('ffplay {0}.tmp -autoexit -loglevel warning'\
+        music = Popen('ffplay {0}.tmp -autoexit -loglevel warning'\
                                  .format(play_order))#то включаем
         #какая там сейчас должна быть песня.
         playing = True#И громко всем об этом заявим!
+        
+    sleep(0.05)#И подождём немного, прежде чем повторять, чтоб не грузить комп.
